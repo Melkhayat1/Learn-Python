a16cd8dc99755ff6e1462723a2394e42836152b1
Mohamed <karate.mohammed@yahoo.com>
2026-07-16 14:14:05 +0300

    Decrease Loop Range

--- Learn 1.py
+++ Learn 1.py
@@ -1,6 +1,6 @@
 print("Hello world "*3)
 
-for i in range(2):
+for i in range(1):
     print("Hello world "*2)
 
 
--- Learn 2.py
+++ Learn 2.py
@@ -1,6 +1,6 @@
 print("Hello world "*3)
 
-for i in range(3):
+for i in range(2):
     print("Hello world "*3)
 
 
